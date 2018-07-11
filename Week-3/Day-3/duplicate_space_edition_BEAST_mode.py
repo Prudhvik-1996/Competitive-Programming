6625ae949bbf230639ab0bc73490f8a1010adb8e
Prudhvik-1996 <31123713+Prudhvik-1996@users.noreply.github.com>
2018-07-11 10:36:32 +0530

    Update duplicate_space_edition_BEAST_mode.py

--- Week-3/Day-3/duplicate_space_edition_BEAST_mode.py
+++ Week-3/Day-3/duplicate_space_edition_BEAST_mode.py
@@ -1,13 +1,42 @@
 def find_duplicate(arr):
 
     # Find a number that appears more than once ... in O(n) time
-    for i in range(0, len(arr)):
-        if arr[abs(arr[i])] >= 0:
-            arr[abs(arr[i])] = -arr[abs(arr[i])]
-        else:
-            return abs(arr[i])
+#     for i in range(0, len(arr)):
+#         if arr[abs(arr[i])] >= 0:
+#             arr[abs(arr[i])] = -arr[abs(arr[i])]
+#         else:
+#             return abs(arr[i])
 
-    return 0
+#     return 0
+
+    n = len(list)
+    i = n
+    j = n
+    while True:
+#         print("looking for cycle: i %s j %s" % (i, j))
+        i = list[i - 1]  # tortoise
+        j = list[j - 1]  # hare
+        j = list[j - 1]  # hare
+        if i == j:
+#             print("cycle found at %s" % i)
+            break
+
+    # we found a cycle
+    # now restart j
+    # and loop until j meets i again
+    # and that's the start of the cycle (or the dup)
+
+    j = n
+    while True:
+#         print("looking for dup: i %s j %s" % (i, j))
+        i = list[i - 1]
+        j = list[j - 1]
+        if i == j:
+#             print("dup found at %s" % i)
+            break
+
+#     print("dup is %s" % i)
+    return i
 
 # Tests
 
